b899741528ce8b8f49fadb4205b4c93cf3cf6a91
tangbing <tangbing@chinasosmart.com>
2019-07-04 14:51:02 +0800

    获取微信登录二维码

--- start/mac/start.go
+++ start/mac/start.go
@@ -4,12 +4,6 @@ import (
 	"github.com/go-wechat-bot/wx/core/service"
 )
 
-<<<<<<< HEAD
 func main(){
 	service.Start()
 }
-=======
-func main() {
-	fmt.Println("aaaa")
-}
->>>>>>> c445d3d66c635bfd377d13984076d850d5523388
